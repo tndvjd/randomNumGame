8edb72a86d63fb1b8ddf029a1bbbc6c7c07e484d
Hyperidiot <tndvjd@gmail.com>
2021-02-09 17:46:35 +0900

    initial commit

---
+++ randomNumGame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  randomNumGame
+//
+//  Created by 최동균 on 2021/02/09.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
